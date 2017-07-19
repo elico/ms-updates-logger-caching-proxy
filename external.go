83a542548387f7ca693a209b764341489a51f347
Eliezer Croitoru <eliezer@ngtech.co.il>
2017-07-19 19:00:45 +0300

    Code cleanup and addition of some comments

--- external.go
+++ external.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-//	"net/url"
+	//	"net/url"
 	"strings"
 )
 
@@ -13,16 +13,22 @@ func msUpdatesDomainRequest(req *http.Request) bool {
 	if strings.HasSuffix(req.Host, "download.microsoft.com") {
 		return true
 	}
+	// Ms domains:
+	// - .sfx.ms
+	
 	return false
 }
 
 func cachableUpdatesRequest(req *http.Request) bool {
-	if ((req.Method == "GET" ||( cacheHead && req.Method == "HEAD") )&& msUpdatesDomainRequest(req)) {
+	if (req.Method == "GET" || (cacheHead && req.Method == "HEAD")) && msUpdatesDomainRequest(req) {
 		switch {
 		// Blacklisting any option to cache antivirus definntions updates
 		case (strings.Contains(req.URL.Path, "/DefinitionUpdates/")):
 			return false
 		case (strings.HasPrefix(req.URL.Path, "/d/") || strings.HasPrefix(req.URL.Path, "/c/")):
+			// I am not caching filestreamingservice to allow microsoft streaming files when the cache admins do bad things.
+			// If the network admin want's to block winodows updates he will need to know what he is doing.
+			//case (strings.HasPrefix(req.URL.Path, "/d/") || strings.HasPrefix(req.URL.Path, "/c/") || strings.HasPrefix(req.URL.Path, "/filestreamingservice/")):
 			return true
 		case (strings.HasSuffix(req.URL.Path, ".exe") || strings.HasSuffix(req.URL.Path, ".cab") || strings.HasSuffix(req.URL.Path, ".msi") || strings.HasSuffix(req.URL.Path, ".psf") || strings.HasSuffix(req.URL.Path, ".esd") || strings.HasSuffix(req.URL.Path, ".msu")):
 			return true
