9e8161b24a0fdc17e393337d2efbb794a1e6a587
Eliezer Croitoru <eliezer@ngtech.co.il>
2017-07-19 19:16:04 +0300

    Added an lru-cleanup example script which is base on bash and find

---
+++ lru-cleanup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+STOREFOLDER=/var/storedata/
+OLDERTHEN=30
+
+function cleanup() {
+	echo "Cleaning up $1 , for files older then $2"
+	if [ -d "$1" ];then
+		find $1 -type f -atime -$2 -print |xargs -l1 rm -v
+	else
+		echo "$1 MISSING! , Aborted cleanup"
+	fi
+}
+
+if [ -d "$STOREFOLDER" ];then
+	echo "$STOREFOLDER exists"
+else
+	echo "$STOREFOLDER  MISSING!"
+	exit
+fi
+
+case $1 in 
+	requests)
+		echo "cleaning up requets"
+		cleanup $STOREFOLDER/request/ $OLDERTHEN
+	;;
+	responses)
+	echo "responses"
+		cleanup $STOREFOLDER/header/ $OLDERTHEN
+		cleanup $STOREFOLDER/body/ $OLDERTHEN
+	;;
+	*)
+	echo "The options are (requests|responses)"
+	;;
+esac
